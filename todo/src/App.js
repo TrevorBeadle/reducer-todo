defb518d44b72125c5de5a22d310e6fe03d61da2
Trevor Beadle <trevor.beadle.1996@gmail.com>
2020-10-05 12:45:02 -0500

    added and built out reducer and built the form to add todos using said reducer

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useState } from "react";
+import { initialState, reducer } from "./reducers/reducer";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const onChange = e => {
+    setInputValue(e.target.value);
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    dispatch({ type: "ADD_TODO", item: inputValue });
+    setInputValue("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="todo">Add a Todo</label>
+        <input
+          type="text"
+          id="todo"
+          name="todo"
+          onChange={onChange}
+          value={inputValue}
+        />
+        <button>Submit</button>
+      </form>
+
+      {state.map(todo => {
+        return <p>{todo.item}</p>;
+      })}
     </div>
   );
 }
